a9249908728da5c7234923aa50c3111681b2c41e
hype612 <74210816+hype612@users.noreply.github.com>
2021-03-22 17:37:26 +0100

    Update init.cpp

--- GameProject/source/init.cpp
+++ GameProject/source/init.cpp
@@ -1,37 +1,46 @@
 #include "init.h"
-#include "header.h"
 
+
+
+
+std::vector<std::string> FileToStringVector(const std::string filepath)
+{
+    std::vector<std::string> oVector;
+    std::string line;
+    std::ifstream inFile(filepath);
+    while (std::getline(inFile, line))
+    {
+        oVector.push_back(line);
+    }
+    return oVector;
+}
 
 std::vector<std::string> splitString(const std::string& splitted,
- char delimeter)
+    char delimeter)
 {
     std::vector<std::string> tokens;
     std::string token;
     std::istringstream tokenStream(splitted);
-    while(std::getline(tokenStream, token, delimeter))
+    while (std::getline(tokenStream, token, delimeter))
     {
         tokens.push_back(token);
     }
     return tokens;
 }
 
-void playerInit()
-{
-	Player* player = new Player("Miskunn", 100, -10, 50, 1.0);
-	Item SwordOfTruth("Sword of Truth", -99, (Item::itemType)0);
-	player->equip(SwordOfTruth);
-}
+//commented out for tests sake 
 
 
-std::vector<std::string> fileInit(const std::string filepath)
-{
-    std::vector<std::string> oVector;
-	std::string line;
-	std::ifstream initFile(filepath);
-	while(std::getline(initFile, line))
-	{
-        std::getline(initFile, line);
-		oVector.push_back(line);
-    }
-    return oVector;
-}
+//void playerInit()
+//{
+//    Player* player = new Player("Miskunn", 100, -10, 50, 1.0);
+//    Item SwordOfTruth("Sword of Truth", -99, (Item::itemType)0);
+//    player->equip(SwordOfTruth);
+//}
+
+
+//Enemy enemyInit(std::string& enemyStats)
+//{
+//    std::vector<std::string> stats = splitString(enemyStats, ';');
+//    return Enemy(stats[0], std::stoi(stats[1]), std::stoi(stats[2]), std::stof(stats[3]), std::stoi(stats[4]));
+//}
