46af2aea5581347546c8a562fbeaa548c13276f4
hype612 <74210816+hype612@users.noreply.github.com>
2021-03-23 21:44:33 +0100

    Update Main.cpp

--- GameProject/source/Main.cpp
+++ GameProject/source/Main.cpp
@@ -1,28 +1,16 @@
 #include "header.h"
 #include "classes.h"
 #include "dungeon.h"
-//#include "init.h"
+#include "init.h"
 
 
 void gameLoop()
 {
-    //playerInit();
-    //std::vector<std::string> enemyList(fileInit("enemyList.txt"));
+	//not yet finished
 }
 
 int main()
 {
-	//std::cout <<"You are wandering around in the woods when you suddenly hear a loud roar\nYou go into the direction where you heard the roar from, and there you found a troll\n";
-	//player->spellbook[0].getSpell();
-	//player->spellbook[1].getSpell();
-	//player->equip(SpearOfShojin);
-	//Fight(*player, ForestTroll);
-	//Dungeon::Room<8, 8> testroom("bossroom.txt");
-	//testroom.printRoom();
-	//dungeonWorld::Dungeon ruinsOfAnOldCastle(2);
-	//ruinsOfAnOldCastle.addRoom("room.txt");
-	//ruinsOfAnOldCastle.addRoom("bossroom.txt");
-	//player->enterdungeon(ruinsOfAnOldCastle);
     gameLoop();
     std::cin.get();
 	std::cin.get();
