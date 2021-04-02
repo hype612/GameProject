f1778285f3c9cb2f96a593ae0ac443a9fb4b6692
hype612 <74210816+hype612@users.noreply.github.com>
2021-04-02 13:38:55 +0200

    Update dungeon.h

--- GameProject/source/dungeon.h
+++ GameProject/source/dungeon.h
@@ -4,58 +4,23 @@
 #include "header.h"
 #include "classes.h"
 #include <fstream>
-
-namespace Dungeon {
-
-
-	
-	class Room
-	{
-	public:
-	    enum tileType : char;
-        bool isEncounterRoom;
-		Room(std::string filepath, bool encounter);
-		void roomLoop(Player& player);
-		void printRoom();
-
-	private:
-		std::array<tileType, 8* 8> map;
-	};
-    
-    
-    
-    void dungeonLoop(int roomnumber);
-    
-    /*
-	template<int x, int y>
-	class Room
-	{
-	public:
-
-		enum tileType : char
-		{
-			floor = 0, wall, chest, enemy, prop
-		};
-
-		Room(std::string filepath)
-		{
-			std::string line;
-			std::ifstream roomFile(filepath);
-
-			for (size_t i = 0; i < x; i++)
-			{
-				std::getline(roomFile, line);
-				for (size_t j = 0; j < y; j++)
-				{
-					map[i + j * y] = (tileType)line[j];
-				}
-			}
-		}
+#include "init.h"
 
 
-	private:
-		std::array<tileType, x* y> map;
-	};
-	*/
-}
 
+
+class Room
+{
+public:
+    enum tileType : char;
+	Room(std::string filepath, bool encounter);
+	void roomLoop(Player& player);
+	void printRoom();
+    Room(std::vector<std::string>& layout);
+private:
+	std::array<tileType, 8* 8> map;
+};
+
+
+
+void dungeonLoop(int roomnumber);
