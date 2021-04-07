03ab1214d4893ce5ab858c058499f8f278641067
hype612 <74210816+hype612@users.noreply.github.com>
2021-04-07 17:34:44 +0200

    Update dungeon.h

--- GameProject/source/dungeon.h
+++ GameProject/source/dungeon.h
@@ -15,10 +15,15 @@ public:
     enum tileType : char;
 	Room(std::string filepath, bool encounter);
 	void roomLoop(Player& player);
-	void printRoom();
+	void printRoom(int xpos, int ypos);
     Room(std::vector<std::string>& layout);
+    char enemyCount;
+    char lootCount;
 private:
-	std::array<tileType, 8* 8> map;
+    //old system, now remade
+    //return to this in case it wont work
+	//std::array<tileType, 8 * 8> map;
+    std::array<std::string, 8> map;
 };
 
 
