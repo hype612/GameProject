b389aa2c3b58843b9865e26ec6aee291fe5c0617
hype612 <74210816+hype612@users.noreply.github.com>
2021-04-10 12:05:43 +0200

    Update classes.h

--- GameProject/source/classes.h
+++ GameProject/source/classes.h
@@ -84,7 +84,7 @@ public:
 
     //basic variables of player
 	std::vector<Consumable> inventory;
-	std::array<Item*, 5> equipment { nullptr};
+	std::array<Item*, 5> equipment = {nullptr};
 	std::array<Spell, 2> spellbook { Spell("Fireball", -50, 10, "Do you like to play with fire??"), Spell("Tome of Healing", 30, 20, "A mediocre healing spell for those who are not familiar with the way of magic") };
 	size_t dungeonPos, globalPos;
     int roomPos[2] = {0, 0};
@@ -95,7 +95,7 @@ public:
 
 
     //basic functions of the player
-    void dMove(char direction);
+    void dMove(char direction, std::array<int, 2>& oArray);
 
 
     //item related
