6079c5ffee973ef04f25a3a06f90a89abd6f88a2
hype612 <74210816+hype612@users.noreply.github.com>
2021-05-17 15:52:37 +0200

    Add files via upload

---
+++ GameProject/source/resources.h
@@ -0,0 +1,11 @@
+
+#include "main.h"
+#include "init.h"
+
+
+namespace Resources {
+
+    static std::vector<std::string> enemyList = FileToStringVector("../resources/enemyList.txt");
+    static std::vector<std::string> itemList = FileToStringVector("../resources/itemList.txt");
+    static std::vector<std::string> roomList = FileToStringVector("../resources/roomList.txt");
+}
