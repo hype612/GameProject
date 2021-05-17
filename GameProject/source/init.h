25ba472ca688a77654cd1dfae0d747f057c34ce6
hype612 <74210816+hype612@users.noreply.github.com>
2021-05-17 15:51:21 +0200

    Update init.h

--- GameProject/source/init.h
+++ GameProject/source/init.h
@@ -2,8 +2,11 @@
 #include "classes.h"
 #include <fstream>
 #include <sstream>
+#include <chrono>
+#include <random>
 
 
 std::vector<std::string> FileToStringVector(std::string filepath);
 std::vector<std::string> splitString(const std::string& splitted, char delimeter);
+int randomNumber(int maxNumber);
 void playerInit();
