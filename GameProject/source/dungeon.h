d093c998de3845e0d7c5f55585d5fd4c8787548a
hype612 <74210816+hype612@users.noreply.github.com>
2021-05-17 15:49:39 +0200

    Update dungeon.h

--- GameProject/source/dungeon.h
+++ GameProject/source/dungeon.h
@@ -13,19 +13,31 @@ class Room
 {
 public:
     enum tileType : char;
-	Room(std::string filepath, bool encounter);
+    Room(std::string filepath);
 	void roomLoop(Player& player);
 	void printRoom(int xpos, int ypos);
     Room(std::vector<std::string>& layout);
-    char enemyCount;
-    char lootCount;
+    char enemyCount = 0;
+    char lootCount = 0;
+
 private:
-    //old system, now remade
-    //return to this in case it wont work
-	//std::array<tileType, 8 * 8> map;
     std::array<std::string, 8> map;
 };
 
 
+//dungeons are fixed 5  member arrays of rooms
+class Dungeon
+{
+public:
+    Dungeon(int dSize);
+    void DungeonLoop(Player&);
+private:
+    //for some reason it doesn't work with arrays
+    //it says that the constructor has been deleted
+    //look into it and change into array ASAP
+    std::vector<Room> dungeonMap;
+};
 
-void dungeonLoop(int roomnumber);
+
+
+//void dungeonLoop(int roomnumber);
